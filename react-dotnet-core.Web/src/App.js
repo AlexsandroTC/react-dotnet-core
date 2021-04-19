874babd6ec7b875b490c2239cc4acddc9219c995
Alexsandro Teixeira <alexsandro.tcunha@hotmail.com>
2021-04-19 16:09:55 -0300

    Feat: adicionado conexão em uma api.

--- react-dotnet-core.Web/src/App.js
+++ react-dotnet-core.Web/src/App.js
@@ -1,9 +1,41 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Listar</h1>
-    </div>
-  );
+import { Component } from 'react';
+import api from './api'
+
+
+class App extends Component{
+  state = {
+    filmes:[],
+  }
+
+  async componentDidMount(){
+    const response = await api.get('star%20wars');
+
+    this.setState({filmes: response.data});
+  }
+
+  render(){
+
+    const {filmes} = this.state;
+
+    return (
+      <div>
+        <h1>Listar os filmes</h1>
+        {filmes.map(filme => (
+          <li key={filme.show.id}>
+            <h2>
+              <strong>Titulo do filme:</strong> 
+              {filme.show.name}
+            </h2>
+            <p>
+              <strong>URL do filme</strong>
+              {filme.show.url}
+            </p>
+          </li>
+
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
