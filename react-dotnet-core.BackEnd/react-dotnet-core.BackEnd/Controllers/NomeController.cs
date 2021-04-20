9cedee75928962ec44e3995f98ca54bcd225b80b
Alexsandro Teixeira <alexsandro.tcunha@hotmail.com>
2021-04-20 15:51:11 -0300

    Feat: adicionado end point novo para teste do service bus

---
+++ react-dotnet-core.BackEnd/react-dotnet-core.BackEnd/Controllers/NomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using react_dotnet_core.BackEnd.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace react_dotnet_core.BackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NomeController : ControllerBase
+    {
+        private static readonly string[] Nome = new[]
+{
+            "Lucas", "Diego", "Alexsandro", "Felipe", "Moises"
+        };
+
+        // GET: NomeController
+        [HttpGet]
+        public string[] Get()
+        {
+            return Nome;
+        }
+
+        // Post: ProdutoController/Details/5
+        [HttpPost]
+        public ActionResult Details(int id)
+        {
+            var teste = string.Empty;
+
+            return NoContent();
+        }
+    }
+}
---
+++ react-dotnet-core.BackEnd/react-dotnet-core.BackEnd/Model/Produto.cs
@@ -0,0 +1,8 @@
+﻿namespace react_dotnet_core.BackEnd.Model
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+        public string Descricao { get; set; }
+    }
+}
